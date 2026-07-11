7afcd1afcdf79ca2f310736658c7cc25c52f1869
Rob Johansen <rob.johansen@gmail.com>
2026-07-11 15:16:11 -0600

    gh-64 | Add API for phase 5 hit

--- packages/api/src/libs/phase10/phase5.ts
+++ packages/api/src/libs/phase10/phase5.ts
@@ -1,6 +1,59 @@
 import { logger } from '@/logger'
 import type { Card, Phase, Player } from '@jgames/types'
 
+type HitProps = {
+  added: Card[]
+  cards: Card[]
+  hitteeId: string
+  hitterId: string
+  players: Player[]
+}
+
+export const hit = ({ added, cards, hitteeId, hitterId, players }: HitProps): boolean => {
+  const hitter = players.find((p) => p.id === hitterId)
+  if (!hitter) {
+    logger.error('Error hitting phase 5 (hitter not found)')
+    return false
+  }
+
+  const hittee = players.find((p) => p.id === hitteeId)
+  if (!hittee) {
+    logger.error('Error hitting phase 5 (hittee not found)')
+    return false
+  }
+
+  const hitterCards = hitter.cards as Card[]
+  const hitteeCards = (hittee.played as Phase<5>).run8
+
+  // Remove the `added` cards from `hitterCards`
+  for (let i = hitterCards.length - 1; i >= 0; i--) {
+    const handCard = hitterCards[i]
+
+    for (let j = added.length - 1; j >= 0; j--) {
+      const hitCard = added[j]
+
+      if (handCard.color === hitCard.color && handCard.value === hitCard.value) {
+        hitterCards.splice(i, 1)
+        added.splice(j, 1)
+        break
+      }
+    }
+  }
+
+  if (added.length !== 0) {
+    logger.error('Error hitting phase 5 (run8 move problem)')
+    return false
+  }
+
+  // Remove all cards from `hitteeCards` and replace them with those in `cards`
+  hitteeCards.length = 0
+  for (const card of cards) {
+    hitteeCards.push(card)
+  }
+
+  return true
+}
+
 export const play = (phase: Phase<5>, userId: string, players: Player[]): boolean => {
   const player = players.find((p) => p.id === userId)
   if (!player) {
